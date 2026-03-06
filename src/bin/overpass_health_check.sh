38481e4d02eddcea4fb0720a8713e7a37191029e
Andy Townsend <ajtown@h24.atownsend.org.uk>
2026-03-06 14:27:06 +0000

    Added src/bin/overpass_health_check.sh to run daily.

---
+++ src/bin/overpass_health_check.sh
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------
+# Daily Overpass health check
+# overpass_health_check.sh
+# ----------------------------------------------------------------------------
+echo "Overpass health check:"
+hostname
+dnsdomainname
+echo "Disk space and usage:"
+df .
+cd /opt/op
+du -s * | sort -n -r 
+echo
+echo "fetched_osc status:"
+ls -alt diff
+grep timestamp diff/state.txt
+echo
+echo "Processes:"
+ps -ef | grep "/op/" | grep -v grep
+#
